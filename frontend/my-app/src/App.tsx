5a516ff77d4b2af5e229b95449b6f626de95301e
David Fryd <david@fryd.com>
2022-09-03 15:29:32 -0400

    frontend progress

--- frontend/my-app/src/App.tsx
+++ frontend/my-app/src/App.tsx
@@ -1,24 +1,48 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
+  // The set of strings displayed as a list
+  const [stringList, setStringList] = React.useState([""]);
+
+  // Get the list of strings from the backend
+  // React.useEffect(() => {
+
+  function handleClick() {
+    console.log("clicked!");
+
+    const requestOptions: RequestInit = {
+      method: "POST",
+      body: JSON.stringify({ key: "Stringtoadd!" }),
+    };
+
+    // Send a POST request to the backend to add a string to the set
+    fetch("http://localhost:3017/addtoset", requestOptions)
+      .then((response) => response.json())
+      .then((data) => {
+        console.log(data);
+        return data;
+      })
+      .then((data) => {});
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="">
+        <div className="inputSection">
+          <label>Add a string to the stringset:</label>
+          <input type="text"></input>
+          <button onClick={handleClick}>Add the string to the set</button>
+        </div>
+        <div className="displaySection">
+          <br />
+          Current string set: <br />
+          {stringList.map((value) => (
+            <span key={"val:" + value}>{value + " "}</span>
+          ))}
+        </div>
+      </div>
     </div>
   );
 }

78f52df3177e686e0a10987120e84af5082f076e
David Fryd <david@fryd.com>
2022-09-03 19:45:39 -0400

    frontend v1 functional

--- frontend/my-app/src/App.tsx
+++ frontend/my-app/src/App.tsx
@@ -3,28 +3,41 @@ import logo from "./logo.svg";
 import "./App.css";
 
 function App() {
+  // The current user input of the text field
+  const [userInput, setUserInput] = React.useState("");
   // The set of strings displayed as a list
   const [stringList, setStringList] = React.useState([""]);
 
-  // Get the list of strings from the backend
-  // React.useEffect(() => {
+  const getSetEndpoint = "http://localhost:3017/getset";
+  const addToSetEndpoint = "http://localhost:3017/addtoset";
 
-  function handleClick() {
-    console.log("clicked!");
 
+  function getSetFromServer() {
+    fetch(getSetEndpoint)
+      .then((response) => response.json())
+      .then((data) => {setStringList(data.stringSetAsList)})
+  }
+
+  // Get the list of strings from the backend on page load
+  React.useEffect(() => {
+    getSetFromServer();
+  }, []);
+
+  function handleSubmitString() {
     const requestOptions: RequestInit = {
       method: "POST",
-      body: JSON.stringify({ key: "Stringtoadd!" }),
+      // Use the input of the text field for the body of the request
+      body: JSON.stringify({ key: userInput }),
     };
-
     // Send a POST request to the backend to add a string to the set
-    fetch("http://localhost:3017/addtoset", requestOptions)
+    fetch(addToSetEndpoint, requestOptions)
       .then((response) => response.json())
       .then((data) => {
         console.log(data);
         return data;
       })
-      .then((data) => {});
+      // After the POST request is complete, get the updated set from the backend
+      .then(() => getSetFromServer());
   }
 
   return (
@@ -32,14 +45,19 @@ function App() {
       <div className="">
         <div className="inputSection">
           <label>Add a string to the stringset:</label>
-          <input type="text"></input>
-          <button onClick={handleClick}>Add the string to the set</button>
+          <input
+            type="text"
+            value={userInput}
+            onChange={(e) => setUserInput(e.target.value)}
+          ></input>
+          <br />
+          <button onClick={handleSubmitString}>Add the string to the set</button>
         </div>
         <div className="displaySection">
           <br />
           Current string set: <br />
-          {stringList.map((value) => (
-            <span key={"val:" + value}>{value + " "}</span>
+          {stringList.map((value, index) => (
+            <span key={"val:" + value}>{(index ? ", " : " ") + "\""+ value + "\""}</span>
           ))}
         </div>
       </div>
